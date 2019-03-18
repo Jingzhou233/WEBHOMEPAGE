269369ea6130e7b23622d04738cf39d419f6f2cb
Jingzhou233 <47308691+Jingzhou233@users.noreply.github.com>
2019-03-17 17:49:36 -0700

    add coding file
    html css js

---
+++ JS/main.js
@@ -0,0 +1,32 @@
+
+$(window).bind('scroll',function(e){
+    parallaxScroll();
+    example();
+});
+
+function parallaxScroll(){
+var scrolled = $(window).scrollTop();
+ 
+$(".backgroundimg").css('top',(0-(scrolled*.3))+'px');
+$(".columnb").css('top',(0-(scrolled*.25))+'px');
+    $(".columnc").css('top',(0-(scrolled*.17))+'px');
+    $(".columnd").css('top',(0-(scrolled*.15))+'px');
+  
+$(".b1").css('color','#eb2f2f');
+  $(".b2").css('color','black');
+  if(scrolled>2800){
+    $(".line").css('top',(0-(scrolled*.15))+'px');
+    $(".b1").css('color','black');
+    $(".b2").css('color','#eb2f2f');
+  }
+
+}
+
+
+
+
+function example() {
+  var tempScrollTop = $(window).scrollTop();
+  console.log("Scroll from Top: " + tempScrollTop.toString());
+};
+
